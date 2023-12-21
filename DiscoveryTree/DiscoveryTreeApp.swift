bd1d60ccb86319c8fb4471215b13d1e8e6cbb93c
Ricky Munz <ricky.munz@gmail.com>
2023-12-21 14:53:02 -0500

    Create Xcode project.

---
+++ DiscoveryTree/DiscoveryTreeApp.swift
@@ -0,0 +1,17 @@
+//
+//  DiscoveryTreeApp.swift
+//  DiscoveryTree
+//
+//  Created by Ricky Munz on 12/21/23.
+//
+
+import SwiftUI
+
+@main
+struct DiscoveryTreeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
